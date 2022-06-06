14e4650cf4d6a4f00121d71edd91f4af3c8dd5bd
talha shaikh <talha@192.168.1.12>
2022-06-06 09:17:52 +0530

    Kubectl and helm installed on remote

--- remote/remote-exec.tf
+++ remote/remote-exec.tf
@@ -4,6 +4,14 @@ provider "aws" {
   
 }
 
+resource "aws_eip_association" "tf_eip_assoc" {
+  instance_id   = aws_instance.first-ec2.id
+  allocation_id = "eipalloc-04b564d4386acfcb8"
+  depends_on = [
+    aws_instance.first-ec2
+  ]
+}
+
 resource "aws_instance" "first-ec2" {
   ami           = "ami-0756a1c858554433e" # ap-south-1
   instance_type = "t2.micro"
@@ -20,15 +28,23 @@ resource "aws_instance" "first-ec2" {
       host = self.public_ip
   }
 
-  provisioner "local-exec" {
-    command = "touch test_local-exec.txt"
-  }
+  # provisioner "local-exec" {
+  #   command = "touch test_local-exec.txt"
+  # }
   
   provisioner "remote-exec" {
     inline = [
 	  "sudo apt-get update",
-    "sudo apt-get install apache2 -y",
-	  "sudo systemctl start apache2",
+    "curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl",
+    "curl -LO https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256",
+    "echo $(cat kubectl.sha256)  kubectl | sha256sum --check",
+    "sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl",
+    "kubectl version --client",
+    "curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null",
+    "sudo apt-get install apt-transport-https --yes",
+    "echo deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list",
+    "sudo apt-get update",
+    "sudo apt-get install helm"
     ]
   }
   
